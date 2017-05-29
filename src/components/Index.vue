53b6bfe2e5007f69ce4c2bb7f8631cef102cddb6
Damien Stanton <damien@blinkanalytics.com>
2017-05-29 11:42:30 -0400

    live reload works 👍

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,7 @@
   <q-layout>
     <div slot="header" class="toolbar">
       <q-toolbar-title :padding="0">
-        Quasar Framework v{{$q.version}}
+        InstaCap 
       </q-toolbar-title>
     </div>
 
